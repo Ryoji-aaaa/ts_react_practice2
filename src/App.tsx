89f7078946d6d350efa82f03b862caad5b8f0772
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-09-22 16:55:36 +0900

    Header,CourseGoal関数の作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CourseGoal from "./components/CourseGoal";
+// import "./App.css";
+import goalsImg from "./assets/goals.jpg"
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <Header image={{src:goalsImg,alt :"A list of goals"}}>
+          <h1>Your Course Goals</h1>
+        </Header>
+        <CourseGoal title="Learn React + TS">
+          <p>Learn it from the ground up</p>
+        </CourseGoal>
+        <CourseGoal title="Learn React + TS">
+          <p>aaaa</p>
+        </CourseGoal>
+      </main>
+      <p className="kai"></p>
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,33 @@
+import { type PropsWithChildren } from "react";
+
+
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+// export default CourseGoal;
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({title,children}: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
