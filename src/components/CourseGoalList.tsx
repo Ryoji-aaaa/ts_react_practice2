b1aa1c89d08bbc660209f4f1d8f2146c528d7ee4
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-10-20 10:21:30 +0900

    InfoBoxの実装

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,22 +1,48 @@
 import CourseGoal from "./CourseGoal";
-import {type CourseGoal as CGoal } from "../App"
+import { type CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalListProps = {
   goals: CGoal[];
-  onDeleteGoal: (id:number) => void ;
+  onDeleteGoal: (id: number) => void;
 };
 
-export default function CourseGoalList({ goals ,onDeleteGoal}: CourseGoalListProps) {
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You have no goals yet. Startadding some!</InfoBox>
+    );
+  }
+
+  let warinigBox : ReactNode;
+  if (goals.length >=4) {
+    warinigBox = (
+      <InfoBox mode="warning" severity="high">
+        You have a lot of goals. Consider removing some of them
+      </InfoBox>
+    );
+  }
 
   return (
-    <ul>
-      {goals.map((goals) => (
-        <li key={goals.id}>
-          <CourseGoal id={goals.id} title={goals.title} onDelete = {onDeleteGoal}>
-            <p>{goals.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+    {warinigBox}
+      <ul>
+        {goals.map((goals) => (
+          <li key={goals.id}>
+            <CourseGoal
+              id={goals.id}
+              title={goals.title}
+              onDelete={onDeleteGoal}
+            >
+              <p>{goals.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
